e6fa892ef0a392df0b06022fd2cdaff66a4a2f2a
apexa11 <apexachoxi7@gmail.com>
2017-05-30 16:17:08 -0400

    blog

--- main.py
+++ main.py
@@ -1,43 +1,100 @@
-import os
 import re
 from string import letters
+import hmac
 
 import webapp2
-import jinja2
-
 from google.appengine.ext import db
 
-#set  template directory jinja env for templates
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-                               autoescape = True)
+import helper
 
-secret = 'du.uyx^Ed~ppQ12d'
-
-def render_str(template,**params):
-    t = jinja_env.get_template(template)
-    return t.render(params)
+secret = 'happy'
 
 def make_secure_val(val):
-    return '%s|%s' %(val, hmac.new(secret,val).hexdigest)
+    """
+        create secure value using secret
+    """
+    return '%s|%s' %(val, hmac.new(secret,val).hexdigest())
 
 def check_secure_val(secure_val):
+    """
+     verify secure value against secret
+    """
     val= secure_val.split(|)[0]
     if secure_val == make_secure_val(val)
      return val
 
 class Handler(webapp2.RequestHandler):
+    """
+     This Handler class, inherites from webapp2.RequestHandler
+     and provides helper method
+    """
+    def render_str(self, template, **params):
+        """
+            This methods renders html using template.
+        """
+        params['user'] = self.user
+        return helper.jinja_render_str(template, **params)
+
     def render(self,template, **kw):
         self.response.out.write(render_str(template, **kw))
 
     def write(self,*a,**kw):
+        """
+         This methods write output to client browser.
+        """
         self.response.out.write(*a,**kw)
 
+
     def set_secure_cookie(self,name,val):
+        """
+            set secure cookie to browser.
+        """
         cookie_val= make_secure_val(val)
         self.response.headers.add_header(
             'Set-Cookie',
             '%s=%s; Path=/' %(name,cookie_val))
+
+
+    def read_secure_cookie(self,name,val):
+        """
+         Read secure cookie to browser
+        """
+        cookie_val=self.request.cookies.get(name)
+        return cookie_val and make_secure_val(cookie_val)
+
+    def login(self,user):
+        """
+            verifies user existance.
+        """
+        self.set_secure_cookie('user_id',str(user.key().id()))
+
+    def logout(self):
+        """
+            remove all imformation from cookies
+        """
+        self.response.headers.add_header('Set-Cookie','user_id=; Path=/')
+
+    def initialize(self,*a,**kw):
+        """
+            This methods gets executed for each page and
+            verfies user login status, using cookie information.
+        """
+        webapp2.RequestHandler.initialize(self,*a,**kw)
+        uid = self.read_secure_cookie('user_id')
+        self.user = uid and User.by_id(int(uid))
+
+    def blog_key(name='default'):
+        return db.Key.from_path('blog',name)
+
+    class BlogFront(Handler):
+        def get(self):
+        """
+            this renders home page with all post ,sort by Date
+        """
+        posts = db.GqlQuery("select * from Post order by created desc limit 10")
+        deleted_post_id = self.request.get('deleted_post_id')
+        self.render('front.html',posts=posts,deleted_post_id=deleted_post_id)
+
 
 
 
@@ -52,6 +109,8 @@ def valid_password(password):
 EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
 def valid_email(email):
     return  email and EMAIL_RE.match(email)
+
+
 
 
 class Signup(Handler):
