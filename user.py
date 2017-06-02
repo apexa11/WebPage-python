1c813ebac35d724ddecae7163d0b025683f6f315
apexa11 <apexachoxi7@gmail.com>
2017-06-02 12:15:32 -0400

    fixes

---
+++ user.py
@@ -0,0 +1,20 @@
+import random
+import hashlib
+
+from string import letters
+from google.appengine.ext import db
+
+def make_salt(length=5):
+    return ''.join(random.choice(letters) for x in xrange(length))
+
+
+def make_pw_hash(name, pw, salt=None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s,%s' % (salt, h)
+
+
+def valid_pw(name, password, h):
+    salt = h.split(',')[0]
+    return h == make_pw_hash(name, password, salt)
